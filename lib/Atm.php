4c789dc95920384ccc195ff8e17f52da0b76fe40
Rob <rob@Robs-iMac.local>
2014-07-02 11:27:27 +1000

    Initiate autoload by defining the namespace and unitTest

--- lib/Atm.php
+++ lib/Atm.php
@@ -1,4 +1,6 @@
 <?php
+namespace Atm;
+
 class Atm {
 
     private $fifties;
--- test/lib/AtmTest.php
+++ test/lib/AtmTest.php
@@ -6,10 +6,18 @@
  * Time: 10:19 AM
  * To change this template use File | Settings | File Templates.
  */
+require dirname(__FILE__).'/../../vendor/autoload.php';
+
 class AtmTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testInstanceOfAtm(){
-        echo 'Here I am';
+
+        $oInstance = \Atm\Atm::getInstance(3,4);
+
+        $this->assertTrue(($oInstance instanceof \Atm\Atm));
     }
+
+
+    
 }
